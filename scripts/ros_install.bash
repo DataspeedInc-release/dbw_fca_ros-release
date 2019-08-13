e75bdec0ae529842661aeed448f26a51d4c72ce5
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2019-08-13 15:21:24 -0400

    Rebase from 'release/melodic/dbw_fca'

--- scripts/ros_install.bash
+++ scripts/ros_install.bash
@@ -43,7 +43,7 @@ fi
 # Install ROS
 echo "Installing ROS $ROS_DISTRO..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y ros-$ROS_DISTRO-desktop
 sudo rosdep init
@@ -57,11 +57,11 @@ echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
 # Install SDK
 echo "Installing SDK..."
-bash <(wget -q -O - https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/default/dbw_fca/scripts/sdk_install.bash)
+bash <(wget -q -O - https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/master/dbw_fca/scripts/sdk_install.bash)
 
 # Configure startup script
 mkdir -p $HOME/.config/autostart
-wget -q https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/default/dbw_fca/scripts/joystick_demo.desktop -O $HOME/.config/autostart/joystick_demo.desktop
+wget -q https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/master/dbw_fca/scripts/joystick_demo.desktop -O $HOME/.config/autostart/joystick_demo.desktop
 
 ### Misc fixes ###
 # Fix launcher icons
